b2228ca535500cb8e2b9a069ce7698fca0ac31bc
J T <julka.m.t@gmail.com>
2021-09-01 11:49:16 +0200

    cloud_cover_0_10.py

--- cloud_cover_0_10.py
+++ cloud_cover_0_10.py
@@ -1,9 +1,5 @@
 import pandas as pd
 
-master_OT = pd.read_csv('E:\master_OT.csv')
-master_OT_cloud_010 = (master_OT[(master_OT['Scene Cloud Cover L1'] < 10)])
-master_OT_cloud_010.to_csv(r'E:\master_OT_cloud_010.csv')
-
 def cloud_cover_0_10 (file_path, res_filename):
 
     master = pd.read_csv(file_path)
